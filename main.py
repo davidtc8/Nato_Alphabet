b018dc7316617bd0e359dbd007b2e558e1c10ed2
davidtc8 <davidtorresc8@gmail.com>
2021-02-15 12:51:10 -0600

    Nato Alphabet Code!

--- main.py
+++ main.py
@@ -8,17 +8,22 @@ df = pd.read_csv(r"nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
-name = input("Enter your name: ").upper()
-try:
-    nato_alphabet = [nato_dict[letter] for letter in name]
-    #words_in_name = [letter.title() for letter in name]
-    #nato_alphabet = [row for word in words_in_name for (index, row) in nato_dict.items() if word in row]
-    print(nato_alphabet)
-except KeyError:
-    print("Sorry, only letters in the alphabet Unu")
+def generate_phonetic():
+    name = input("Enter your name: ").upper()
+    try:
+        nato_alphabet = [nato_dict[letter] for letter in name]
+        #words_in_name = [letter.title() for letter in name]
+        #nato_alphabet = [row for word in words_in_name for (index, row) in nato_dict.items() if word in row]
+        print(nato_alphabet)
+    except KeyError:
+        print("Sorry, only letters in the alphabet Unu")
+        generate_phonetic()
+
 
 # Long way
 #for word in words_in_name:
 #    for (index, row) in nato_dict.items():
 #        if word in row:
 #            print(row)
+
+generate_phonetic()
